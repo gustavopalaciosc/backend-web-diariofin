ab54507a22f20d6c047f60b88cfe5165b2e91ae9
gustavopalaciosc <gpalaciosc@uc.cl>
2024-06-10 00:09:38 -0400

    fix(app): fix app structure

--- app/main.py
+++ app/main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
 from .utils.article_scrapper import articleScrapper
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get('/')
+async def get():
+    return {"message": "Saludos desde app!"}
 
 # http://127.0.0.1:8000/article/?url=https://www.df.cl/economia-y-politica/macro/las-senales-economicas-en-mayo-se-modera-el-interes-por-el-consumo
 @app.get('/article/')
