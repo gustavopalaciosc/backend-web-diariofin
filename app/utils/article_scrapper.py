cb7007749a0b831a227810c713dcab744b411ec3
gustavopalaciosc <gpalaciosc@uc.cl>
2024-06-12 18:18:08 -0400

    fix(utils/article_scrapper.py): add request header

--- app/utils/article_scrapper.py
+++ app/utils/article_scrapper.py
@@ -3,7 +3,8 @@ from bs4 import BeautifulSoup as bs
 
 
 def articleScrapper(url):
-        html_content  = requests.get(url, allow_redirects=False).text
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+        html_content  = requests.get(url, allow_redirects=False, headers=headers).text
         soup = bs(html_content, 'html.parser')
         title = soup.find('h1', class_='titular').text
         subtitle = soup.find('h2', class_='bajada').text
